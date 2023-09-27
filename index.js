8d6ddaa829577e03c3dd6f9694c0ab2353ca8137
saurav <usaurav29@gmail.com>
2023-09-27 20:17:43 +0530

    Personal Portfolio Website

---
+++ index.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "right") {
+      x.className += " responsive";
+    } else {
+      x.className = "right";
+    }
+  }
+
+  
